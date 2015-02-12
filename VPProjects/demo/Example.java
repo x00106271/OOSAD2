a96cf637d7a0255df3341ced8082d6946677aa1b
x00106271 <x00106271@outlook.com>
2015-02-12 12:14:03 +0000

    week3 work

---
+++ VPProjects/demo/Example.java
@@ -0,0 +1,26 @@
+package demo;
+
+public class Example {
+
+	private string name;
+
+	public Example() {
+		// TODO - implement Example.Example
+		throw new UnsupportedOperationException();
+	}
+
+	public void getName() {
+		// TODO - implement Example.getName
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param string
+	 */
+	public void setName(int string) {
+		// TODO - implement Example.setName
+		throw new UnsupportedOperationException();
+	}
+
+}
